d83fe300a927b8691d726d1c8174eeb5cc80cfde
tienminhtran <tientot36@gmail.com>
2025-04-11 21:43:13 +0700

    update 7

--- WEEK7/Ex9/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "✅ Xin chào tôi là Tien Minh Tran"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
